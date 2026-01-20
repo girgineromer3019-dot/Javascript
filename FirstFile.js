36d25e95a405bbc9b849e98bc47f10283f8eb2b7
Romer Girginer <girgineromer3019@gmail.com>
2026-01-20 10:15:41 +0100

    Update Js
    I learned how to work Spread Operator

--- FirstFile.js
+++ FirstFile.js
@@ -132,3 +132,8 @@ const [firstColor, secondColor] = colors;
 console.log(firstColor);  // Red
 console.log(secondColor); // Green
 //-------------------------------------------------------------//
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5, 6];
+
+console.log(arr2); // [1, 2, 3, 4, 5, 6]
+//-------------------------------------------------------------//
