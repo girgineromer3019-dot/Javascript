396b4e04756d832eb84f310af889a99d0035bf3e
Romer Girginer <girgineromer3019@gmail.com>
2026-02-01 14:27:14 +0100

    Update Js
    I did Js example

--- FirstFile.js
+++ FirstFile.js
@@ -209,3 +209,17 @@ console.log(document.forms);   // Sayfadaki formları getirir
 console.log(document.links);   // Sayfadaki linkleri getirir
 console.log(document.images);  // Sayfadaki resimleri getirir
 //-------------------------------------------------------------//
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+const person1 = new Person("Omer", 30);
+person1.introduce(); // Hello, my name is Omer and I am 30 years old.
+//-------------------------------------------------------------//
